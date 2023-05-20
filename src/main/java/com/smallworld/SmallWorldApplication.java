efb137a79d341aaaf82e08448b0fa77d209499d9
muhammadanas2 <muhammad.anas@medznmore.com>
2023-05-20 19:59:36 +0500

    init project

---
+++ src/main/java/com/smallworld/SmallWorldApplication.java
@@ -0,0 +1,12 @@
+package com.smallworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallWorldApplication.class, args);
+    }
+}
